816c59c14ec661a3232d8679260fd72701c5434e
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-04-04 17:43:26 +0500

    Little typography changes in the hero section

--- src/components/Hero/HeroImage/HeroImage.vue
+++ src/components/Hero/HeroImage/HeroImage.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="lg:w-4/5 mx-auto mt-8">
-        <div class="flex flex-wrap w-full py-32 px-10 relative mb-4 transition-transform transform hover:scale-95 duration-300 cursor-pointer">
+        <div class="flex flex-wrap w-full py-32 px-10 relative mb-4">
             <img alt="gallery" class="w-full object-cover h-full object-center block absolute opacity-30 inset-0" src="https://picsum.photos/800/400">
             <div class="text-center relative z-10 w-full">
-                <h2 class="text-4xl text-yellow-500 font-extrabold mb-2">Shooting Stars</h2>
-                <p class="leading-relaxed text-gray-300 text-lg">Skateboard +1 mustache fixie paleo lumbersexual.</p>
+                <a href="#" class="text-4xl text-yellow-500 hover:text-yellow-400 font-extrabold">
+                    <h2>Shooting Stars</h2>
+                </a>
+                <p class="mt-2 leading-relaxed text-gray-300 text-lg">Skateboard +1 mustache fixie paleo lumbersexual.</p>
                 <a class="mt-4 bg-indigo-500 py-3 px-5 text-white rounded-lg inline-flex items-center">Learn More
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -14,30 +16,34 @@
         </div>
         <div class="flex flex-wrap flex-col md:flex-row -mx-2 space-y-4 md:space-y-0">
             <div class="px-2 w-full md:w-1/2">
-                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative transition-transform transform hover:scale-95 duration-300 cursor-pointer">
+                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative">
                     <img alt="gallery" class="w-full object-cover h-full object-center block opacity-30 absolute inset-0" src="https://picsum.photos/801/401">
                     <div class="text-center relative z-10 w-full">
-                        <h2 class="text-2xl text-yellow-500 font-extrabold mb-2">Shooting Stars</h2>
-                        <p class="leading-relaxed text-gray-300 text-lg">Skateboard +1 mustache fixie paleo lumbersexual.</p>
-                        <a class="mt-4 bg-indigo-500 py-2 px-4 text-white rounded-lg inline-flex items-center">Learn More
+                        <a href="#" class="text-2xl text-yellow-500 hover:text-yellow-400 font-extrabold mb-2">
+                            <h2>Shooting Stars</h2>
+                        </a>
+                        <p class="mt-1 leading-relaxed text-gray-300 text-lg">Skateboard +1 mustache fixie paleo lumbersexual.</p>
+                        <!-- <a class="mt-4 bg-indigo-500 py-2 px-4 text-white rounded-lg inline-flex items-center">Learn More
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
                             </svg>
-                        </a>
+                        </a> -->
                     </div>
                 </div>
             </div>
             <div class="px-2 w-full md:w-1/2">
-                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative transition-transform transform hover:scale-95 duration-300 cursor-pointer">
+                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative">
                     <img alt="gallery" class="w-full object-cover h-full object-center block opacity-30 absolute inset-0" src="https://picsum.photos/802/402">
                     <div class="text-center relative z-10 w-full">
-                        <h2 class="text-2xl text-yellow-500 font-extrabold mb-2">Shooting Stars</h2>
-                        <p class="leading-relaxed text-gray-300 text-lg">Skateboard +1 mustache fixie paleo lumbersexual.</p>
-                        <a class="mt-4 bg-indigo-500 py-2 px-4 text-white rounded-lg inline-flex items-center">Learn More
+                        <a href="#" class="text-2xl text-yellow-500 hover:text-yellow-400 font-extrabold">
+                            <h2>Shooting Stars</h2>
+                        </a>
+                        <p class="mt-1 leading-relaxed text-gray-300 text-lg">Skateboard +1 mustache fixie paleo lumbersexual.</p>
+                        <!-- <a class="mt-4 bg-indigo-500 py-2 px-4 text-white rounded-lg inline-flex items-center">Learn More
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
                             </svg>
-                        </a>
+                        </a> -->
                     </div>
                 </div>
             </div>
